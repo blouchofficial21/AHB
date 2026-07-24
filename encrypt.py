c34cf3b1d45715fb944cc817abcc328f866adf29
blouchofficial21 <blouchofficial21@gmail.com>
2026-07-24 07:06:07 +0500

    Updated obfuscated file

---
+++ encrypt.py
@@ -0,0 +1,20 @@
+import base64
+import zlib
+
+input_file = "/storage/emulated/0/Blouch54.py"
+output_file = "/storage/emulated/0/blouch.py"
+
+with open(input_file, "rb") as f:
+    data = f.read()
+
+encoded = base64.b64encode(zlib.compress(data)).decode()
+
+stub = f'''import base64,zlib
+exec(zlib.decompress(base64.b64decode("{encoded}")).decode("utf-8"))
+'''
+
+with open(output_file, "w", encoding="utf-8") as f:
+    f.write(stub)
+
+print("[+] Done!")
+print("[+] Output:", output_file)
